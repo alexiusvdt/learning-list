7ce72b8698b0a76769c10e0db59d0c9cd948f985
alexiusvdt <alex.johnson293@gmail.com>
2023-06-20 10:10:04 -0700

    users can create their own topics

--- ll_env/learning_logs/urls.py
+++ ll_env/learning_logs/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     # page to show all topics
     path('topics/', views.topics, name='topics'),
     # detail page for a single topic
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- ll_env/learning_logs/views.py
+++ ll_env/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 # the various routes and how to process & render the right data
 def index(request):
@@ -22,3 +23,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """add a new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST submitted; process
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    # display blank/invalid
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)

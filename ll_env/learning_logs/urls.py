6ec6b573d9d4feeff04f39222c566f1274d3e274
alexiusvdt <alex.johnson293@gmail.com>
2023-06-20 08:46:56 -0700

    add routing/view information for index

---
+++ ll_env/learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Defintes URL patterns for learning_logs"""
+
+# needed to map urls to views
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home. 3args: route, which func to call in views.py, name for url pattern
+    path('', views.index, name='index')
+]

b5ca40138cae58630cab992f0bf317fd740e875f
alexiusvdt <alex.johnson293@gmail.com>
2023-06-20 10:42:26 -0700

    users can edit existing entries

--- ll_env/learning_logs/urls.py
+++ ll_env/learning_logs/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # page for adding a new topic
     path('new_topic/', views.new_topic, name='new_topic'),
-        # new entries must associate with a topic so we need the topic_id arg in the url
+    # new entries must associate with a topic so we need the topic_id arg in the url
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # page for editing an entry
+    path('edit_entry<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]

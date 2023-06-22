d1a9ac0458341c0f8ce10ad566365199c931c96f
alexiusvdt <alex.johnson293@gmail.com>
2023-06-21 18:58:43 -0700

    add display filtering to users own topics & model update

--- ll_env/learning_logs/views.py
+++ ll_env/learning_logs/views.py
@@ -13,7 +13,7 @@ def index(request):
 @login_required
 def topics(request):
     """show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     # when building page that uses data(db query), pass context var to render() as well as the request obj itself
     return render(request, 'learning_logs/topics.html', context)

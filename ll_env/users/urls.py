1485b39d5302b86208a1187ff299dadca2828d19
alexiusvdt <alex.johnson293@gmail.com>
2023-06-21 18:32:56 -0700

    add view & route for registration

--- ll_env/users/urls.py
+++ ll_env/users/urls.py
@@ -1,9 +1,12 @@
 """defines URL patterns for users"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # default auth urls
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    # registration page
+    path('register', views.register, name='register'),
 ]
